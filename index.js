83b6f4ba46f54d65fe5ec9e188e894201134eabd
AhmadYoussef <Ahmad.k.youssef@gmail.com>
2019-07-03 13:40:02 +0200

    sss

--- index.js
+++ index.js
@@ -49,9 +49,9 @@ app.post('/sendmail', (req, res) => {
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'))
+// })
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
