15432c412db7675c0ca0e381ce601e412f2ed661
AhmadYoussef <Ahmad.k.youssef@gmail.com>
2019-07-03 13:47:56 +0200

    sss

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const nodemailer = require('nodemailer');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
