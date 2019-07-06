cbb65f4f870c4bf3cf8b36e7fcf2c751a215f71f
AhmadYoussef <Ahmad.k.youssef@gmail.com>
2019-07-06 15:56:36 +0200

    fixing bug

--- client/src/NavBar/NavBar.js
+++ client/src/NavBar/NavBar.js
@@ -10,20 +10,25 @@ class NavBar extends React.Component {
     state = {
         path: '/'
     }
+    componentDidMount() {
+        let lang = window.location.pathname.substr(1);
+        this.setState({ path: lang })
+    }
     updatePath = (path = '/') => {
         this.setState({ path: path })
     }
     render() {
+        console.log(this.state.path)
         let content = { ...langData.en };
         if (this.state.path === 'de')
             content = { ...langData.de };
-        let mobDesTopggle = <DesktopNav navActive={this.props.navActive} {...content} updatePath={this.updatePath} path={this.state.path} />;
+        let mobDesToggle = <DesktopNav navActive={this.props.navActive} {...content} updatePath={this.updatePath} path={this.state.path} />;
         if (this.props.isMobile) {
-            mobDesTopggle = <SideNavBar {...content} updatePath={this.updatePath} path={this.state.path} />;
+            mobDesToggle = <SideNavBar {...content} updatePath={this.updatePath} path={this.state.path} />;
         }
         return (
             <nav className={classes.navBar} style={{ ...this.props.topPosition }}>
-                {mobDesTopggle}
+                {mobDesToggle}
             </nav>
         );
     }
